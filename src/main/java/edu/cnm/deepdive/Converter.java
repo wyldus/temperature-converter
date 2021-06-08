f6b36c7e17c035d51fa772a227b9e323af7f9690
dagoberto <berto35572@gmail.com>
2021-06-08 16:31:38 -0600

    Catching and handling parse exception

--- src/main/java/edu/cnm/deepdive/Converter.java
+++ src/main/java/edu/cnm/deepdive/Converter.java
@@ -6,11 +6,17 @@ public class Converter {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Type a celesius temprature (blank to quit): ");
-    String userInput = scanner.nextLine();
-    while (!userInput.isEmpty()) {
-      System.out.printf("Farenheit Temperature: %,.2f degrees%nType a celesius temprature (blank to quit): " ,convertC2F(Double.parseDouble(userInput)));
-      userInput = scanner.nextLine();
+      System.out.print("Type a celesius temprature (blank to quit): ");
+        String userInput = scanner.nextLine();
+        while (!userInput.isEmpty()) {
+          try {
+            System.out.printf(
+                "Farenheit Temperature: %,8.2f degrees%nType a celesius temprature (blank to quit): "
+                ,convertC2F(Double.parseDouble(userInput)));
+          } catch (NumberFormatException e) {
+            System.out.printf("Unable to parse \"%s\". Please try again ", userInput );
+          }
+          userInput = scanner.nextLine();
     }
   }
 
